031a76be601a85c4da904155b9da37fcf4db2c9f
wanderson648 <testepedroca@gmail.com>
2025-01-09 18:59:29 -0300

    Inicia desafio tecnico

---
+++ src/test/java/com/temm/desafiotecnico/DesafiotecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temm.desafiotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
