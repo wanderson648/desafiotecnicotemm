4da02421a1ec3ee7d658ec638e1bd7b23d41d7ad
wanderson648 <testepedroca@gmail.com>
2025-01-09 19:24:36 -0300

    Implementa securityConfig base

---
+++ src/main/java/com/temm/desafiotecnico/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.temm.desafiotecnico.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/users").permitAll()
+                            .requestMatchers("/card").permitAll()
+                            .requestMatchers("/transactions").permitAll()
+                            .anyRequest().authenticated();
+                }).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
