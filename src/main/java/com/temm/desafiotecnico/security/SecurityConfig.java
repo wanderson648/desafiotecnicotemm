a91315cd9df615f0b21315189622860efb96e106
wanderson648 <testepedroca@gmail.com>
2025-01-10 16:47:17 -0300

    Implementa security filter e valida token

--- src/main/java/com/temm/desafiotecnico/security/SecurityConfig.java
+++ src/main/java/com/temm/desafiotecnico/security/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.temm.desafiotecnico.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private SecurityUsuarioFilter securityUsuarioFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,11 +22,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/users").permitAll()
-                            .requestMatchers("/cards").permitAll()
-                            .requestMatchers("/transactions").permitAll()
                             .requestMatchers("/auth").permitAll()
                             .anyRequest().authenticated();
-                }).build();
+                })
+                .addFilterBefore(securityUsuarioFilter, BasicAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
